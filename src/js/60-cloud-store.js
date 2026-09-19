f2b9e59dffffa978937b3d0abb43e5e7602faa3d
StudyNoMentor <jairo258@hotmail.com>
2026-09-19 16:01:17 -0300

    hotfix: remove markAllDirty do envio automático

--- src/js/60-cloud-store.js
+++ src/js/60-cloud-store.js
@@ -394,7 +394,10 @@ const CloudStore = {
     try {
       if (SectionSync._seededProfile !== id) {
         SectionSync._seededProfile = id;
-        SectionSync.markAllDirty(id);
+        /* Startup/envio não transforma divergência de hash em edição. Só
+           seções nunca rastreadas entram automaticamente; mutações reais já
+           estão na outbox explícita pelo hook de DB._set. */
+        if (SectionSync.seedUntrackedOnly) SectionSync.seedUntrackedOnly(id);
       }
       await SectionSync.pushDirty(id);
       return SectionSync._dirtyFor(id).size === 0 && !SectionSync._lastError;
